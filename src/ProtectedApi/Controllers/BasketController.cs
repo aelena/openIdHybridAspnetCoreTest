81ba909ab0c82e51f52d42ffe03e64fef5b73432
aelena <antonioelena@outlook.com>
2018-11-15 10:39:56 +0100

    add basket controller

---
+++ src/ProtectedApi/Controllers/BasketController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProtectedApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get() => Ok();
+    }
+}
